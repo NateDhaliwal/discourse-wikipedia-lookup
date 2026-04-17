048386257bdc88c7fc85984e82a8a7e1c56a5889
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-04-17 15:16:31 +0800

    Use for-of loop

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -4,17 +4,16 @@ export default apiInitializer((api) => {
   api.decorateCookedElement(async (post) => {
     const tooltip = api.container.lookup("service:tooltip");
     const wp_wraps = post.querySelectorAll("[data-wrap=\"wikipedia-lookup\"]");
-    console.log(wp_wraps);
+    let wrap_no = 0;
     if (wp_wraps.length > 0) {
-      for (const wrap_no in wp_wraps) {
-        const wrap = wp_wraps[wrap_no];
-        console.log(wrap);
+      for (const wrap of wp_wraps) {
         const search_term = wrap.textContent;
         wrap.id = `wikipedia-lookup-${wrap_no}`;
         // const res = await fetch(`https://en.wikipedia.org/w/rest.php/v1/search/page?q=${search_term}`);
         // const data = await res.json();
         // console.log(data);
         // if (data.pages.length === 0) return;
+        wrap_no++;
       }
     } else {
       return;
