ed25efea9cfde54835a20a19cbab964e58b6f682
NateDhaliwal <nathaniel.shaan@gmail.com>
2026-04-17 16:18:03 +0800

    Remove console.log statement, wrap_no

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -5,28 +5,20 @@ export default apiInitializer((api) => {
   const tooltip = api.container.lookup("service:tooltip");
   api.decorateCookedElement(async (post) => {
     const wp_wraps = post.querySelectorAll("[data-wrap=\"wikipedia-lookup\"]");
-    let wrap_no = 0;
     if (wp_wraps.length > 0) {
       for (const wrap of wp_wraps) {
         const search_term = wrap.textContent;
-        wrap.id = `wikipedia-lookup-${wrap_no}`;
         const data = await getIfCached(search_term);
-        // const res = await fetch(`https://en.wikipedia.org/w/rest.php/v1/search/page?q=${encodeURIComponent(search_term)}`);
-        // let data = await res.json();
-        // if (data["pages"].length === 0) continue; // Exit if no matches, so don't add any styling
         if (data === null) continue; // Exit if no matches, so don't add any styling
-        // data = data["pages"][0];
         wrap.classList.add("wp-lookup");
         const excerpt = data.excerpt.replace(/(<([^>]+)>)/ig, '');
         const content = `Full page at https://wikipedia.org/wiki/${data.key} \n\n ${excerpt}`;
-        console.log(content);
         tooltip.register(wrap, {
           content: content,
           placement: "top",
           fallbackPlacements: ["bottom"],
           triggers: ["hover"],
         })
-        wrap_no++;
       }
     } else {
       return;
